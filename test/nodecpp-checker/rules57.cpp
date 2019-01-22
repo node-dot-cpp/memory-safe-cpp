005d262b0b89278c33a07a3fa3d9d0937db547a4
Marcos Bracco <mb@o-log-n.com>
2019-01-22 16:02:08 -0300

    added rule S5

---
+++ test/nodecpp-checker/rules57.cpp
@@ -0,0 +1,34 @@
+// RUN: nodecpp-checker %s -- -std=c++14 -isystem %S/Inputs | FileCheck %s -check-prefix=CHECK-MESSAGES -implicit-check-not="{{warning|error}}:"
+
+#include <safe_ptr.h>
+#include <functional>
+
+using namespace nodecpp;
+
+
+void my_sort(std::function<void()>) {}
+
+class S57 {
+public:
+    void on(std::function<void()> cb [[nodecpp::may_extend_to_this]]) {}
+
+    void prohibit() {
+
+        int i = 0;
+        auto l = [&i]() {};
+// CHECK-MESSAGES: :[[@LINE-1]]:20: warning: unsafe capture to extend scope
+        this->on(l);
+    }
+
+    void allow() {
+
+        auto l1 = [this]() {};
+        this->on(l1);
+
+
+        int i = 0;
+        auto l2 = [&i]() {};
+        my_sort(l2);         
+    }
+};
+
