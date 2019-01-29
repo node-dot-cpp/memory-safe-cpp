f6c4c2e67718f7ef298edab1f06e4215342900f7
Marcos Bracco <mb@o-log-n.com>
2019-01-29 15:52:26 -0300

    added safe_library.hpp to test folder

---
+++ test/nodecpp-checker/safe_library.hpp
@@ -0,0 +1,23 @@
+
+namespace fmt {
+namespace v5 {
+	void print();
+}	
+}
+
+namespace nodecpp {
+	void make_owning();
+
+	class safe_ptr;
+	class naked_ptr;
+	class Buffer;
+	namespace net {
+		class Socket;
+		class Server;
+		class Address;
+		class SocketTBase;
+	}
+}
+
+
+
