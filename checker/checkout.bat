b2cd66f3c9539d93317e488fc57b38bb2c169a02
Marcos Bracco <mb@o-log-n.com>
2019-02-14 16:24:18 -0300

    Shallow clone of llvm/clang

--- checker/checkout.bat
+++ checker/checkout.bat
@@ -1,9 +1,9 @@
 md 3rdparty
 cd 3rdparty
 
-git clone -b release_70 https://github.com/llvm-mirror/llvm.git
-git clone -b node-dot-cpp_70 https://github.com/node-dot-cpp/clang.git
-git clone -b release_70 https://github.com/llvm-mirror/clang-tools-extra.git
+git clone --depth 10 -b release_70 https://github.com/llvm-mirror/llvm.git
+git clone --depth 100 -b node-dot-cpp_70 https://github.com/node-dot-cpp/clang.git
+git clone --depth 10 -b release_70 https://github.com/llvm-mirror/clang-tools-extra.git
 
 cd ..
 
