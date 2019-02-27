5d5aff22032a1d8fcaf045d6b5d680b07469ae20
Marcos Bracco <mb@o-log-n.com>
2019-02-27 14:49:12 -0300

    Bug fix of rule 4.1

--- checker/src/nodecpp-checker/nodecpp/NewExprCheck.h
+++ checker/src/nodecpp-checker/nodecpp/NewExprCheck.h
@@ -27,6 +27,7 @@ public:
   NewExprCheck(StringRef Name, ClangTidyContext *Context)
       : ClangTidyCheck(Name, Context) {}
   void registerMatchers(ast_matchers::MatchFinder *Finder) override;
+  bool checkParentExpr(ASTContext *context, const Expr *expr);
   void check(const ast_matchers::MatchFinder::MatchResult &Result) override;
 };
 
