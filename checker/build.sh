4e6ab6469c3d8c5f2cf711e00b21287003d68271
Marcos Bracco <mb@o-log-n-com>
2019-03-14 01:55:00 -0300

    update build.sh

--- checker/build.sh
+++ checker/build.sh
@@ -11,7 +11,7 @@ cd release
 #export CC=clang-7
 #export CXX=clang++-7
 
-cmake -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_EXTERNAL_CHECKER_SOURCE_DIR=${PWD}/../.. -G "Unix Makefiles" ../../3rdparty/llvm
+cmake -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_EXTERNAL_CHECKER_SOURCE_DIR=${PWD}/../.. -G "Unix Makefiles" ../../3rdparty/llvm
 
 make nodecpp-checker
 make nodecpp-safe-library
