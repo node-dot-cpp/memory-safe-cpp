347c6cbd322225e2225a80c8d9fdebad8ea56913
No Bugs' Hare <nobugs@ithare.com>
2019-03-16 10:22:46 +0100

    Replacing non-standard --parents and --recursive with standard -p and -r (didn't work on Mac OS)

--- checker/install.sh
+++ checker/install.sh
@@ -2,8 +2,8 @@
 
 cp build/release/bin/nodecpp-* /usr/local/bin
 
-mkdir --parents /usr/local/lib/clang/7.1.0/include/
-cp --recursive build/release/lib/clang/7.1.0/include/* /usr/local/lib/clang/7.1.0/include/
+mkdir -p /usr/local/lib/clang/7.1.0/include/
+cp -r build/release/lib/clang/7.1.0/include/* /usr/local/lib/clang/7.1.0/include/
 
 
 
