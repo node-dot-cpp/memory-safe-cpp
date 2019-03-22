3ee811b9a274cafd5c399a164d4eee3cb6c0242d
Marcos Bracco <mb@o-log-n.com>
2019-03-21 21:06:45 -0300

    Hack to find std C++ on macOS

--- checker/checkout.bat
+++ checker/checkout.bat
@@ -6,6 +6,7 @@ git clone --depth 10 -b release_70 https://github.com/llvm-mirror/clang.git
 git clone --depth 10 -b release_70 https://github.com/llvm-mirror/clang-tools-extra.git
 
 cd clang
+git reset --hard HEAD
 git apply ..\clang_release_70.diff
 
 cd ..\..
