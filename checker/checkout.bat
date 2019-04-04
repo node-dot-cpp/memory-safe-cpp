47b1a2ea68b742d5b52a461fc120a026bfadd778
Marcos Bracco <mb@o-log-n.com>
2019-04-04 14:33:04 -0300

    checkout scripts now always do a clean checkout. fix #2

--- checker/checkout.bat
+++ checker/checkout.bat
@@ -1,12 +1,15 @@
 
 cd 3rdparty
 
+rmdir /S /Q llvm
+rmdir /S /Q clang
+rmdir /S /Q clang-tools-extra
+
 git clone --depth 10 -b release_70 https://github.com/llvm-mirror/llvm.git
 git clone --depth 10 -b release_70 https://github.com/llvm-mirror/clang.git
 git clone --depth 10 -b release_70 https://github.com/llvm-mirror/clang-tools-extra.git
 
 cd clang
-git reset --hard HEAD
 git apply ..\clang_release_70.diff
 
 cd ..\..
