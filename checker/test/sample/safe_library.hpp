938ca3f76e1d50427b214bceb422508617db199d
Marcos Bracco <mb@o-log-n.com>
2019-11-07 16:03:37 -0300

    safe database update

--- checker/test/sample/safe_library.hpp
+++ checker/test/sample/safe_library.hpp
@@ -2,24 +2,54 @@
 namespace fmt {
 namespace v5 {
 	void print();
+	void format();
 }	
 }
 
 namespace std {
 	void move();
 	void forward();
+	void make_pair();//CHECK
+
+//	class pair;
+	class exception;
+
+	class vector; //TODO
+	class map;
+	class basic_string;
+
+	class _Tree_iterator;
+	class _Vector_iterator;
+
+	void operator==(void*, vector);
 
 	// apple gcc std lib uses namespace __1
 	namespace __1 {
 		void move();
 		void forward();
 	}
+
+	namespace experimental {
+		namespace coroutines_v1 {
+			class coroutine_handle;
+			void operator!=(void*, coroutine_handle);
+		}
+	}
+
 }
 
 namespace nodecpp {
+	void isException();
+	void getException();
+	void setException();
+
+	void setNoException();
+	void setTimeoutForAction();
+	void clearTimeout();
+
 namespace safememory {
 	void make_owning();
-
+	void soft_ptr_static_cast();
 	// osn ptrs are hardcoded with special safety rules
 	// class owning_ptr;
 	// class soft_ptr;
@@ -39,6 +69,13 @@ namespace net {
 	class Server;
 	class Address;
 	class SocketTBase;
+
+
+	class UserDefHandlersWithOptimizedStorage;
+	class ServerBase;
+	class SocketBase;
+
+	void createServer();
 }
 }
 
