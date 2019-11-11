86046ca7638489a1e9437af35e56b90bf6d2b1b9
Marcos Bracco <mb@o-log-n.com>
2019-11-11 12:48:39 -0300

    Update build scripts

--- checker/build.bat
+++ checker/build.bat
@@ -6,8 +6,9 @@ cd build\vs2017
 cmake -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_EXTERNAL_CHECKER_SOURCE_DIR=%cd%\..\.. -Thost=x64 -G "Visual Studio 15 2017 Win64" ..\..\3rdparty\llvm
 
 cmake --build . --target nodecpp-checker --config Release
+cmake --build . --target nodecpp-instrument --config Release
 cmake --build . --target nodecpp-safe-library --config Release
-cmake --build . --target check-nodecpp-checker --config Release
+cmake --build . --target check-nodecpp-tools --config Release
 
 cd ..\..
 
--- checker/build.sh
+++ checker/build.sh
@@ -13,9 +13,10 @@ cd build/release
 cmake -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_EXTERNAL_CHECKER_SOURCE_DIR=${PWD}/../.. -G "Unix Makefiles" ../../3rdparty/llvm
 
 make --jobs=3 nodecpp-checker
+make --jobs=3 nodecpp-instrument
 make --jobs=3 nodecpp-safe-library
 
-make --jobs=3 check-nodecpp-checker
+make --jobs=3 check-nodecpp-tools
 
 cd ../..
 
