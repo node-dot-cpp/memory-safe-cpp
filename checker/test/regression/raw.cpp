7a0bd83b68e0e7f6658181a7b16c35a44b0a66e3
Marcos Bracco <mb@o-log-n.com>
2020-03-03 16:52:35 -0300

    More raw ptr test cases

--- checker/test/regression/raw.cpp
+++ checker/test/regression/raw.cpp
@@ -26,7 +26,38 @@ int* badFunc() {
 }
 
 
-void f(int* p=nullptr);
+void f(int* p = nullptr);
 // CHECK: :[[@LINE-1]]:13: error: (RAW)
 
 
+
+int* badAgain() {
+    int i = 0;
+    int* ip = &i;
+    if(i)
+        return ip;
+//CHECK: :[[@LINE-1]]:16: error: (S5.1)
+    else
+        return &i;
+//CHECK: :[[@LINE-1]]:16: error: (S5.1)
+}
+
+void func51() {
+
+	int i = 0;
+	int* ptr1 = &i;
+	{
+    	int* ptr2 = &i;
+		ptr2 = ptr1; // ok
+		ptr1 = ptr2; // bad
+//CHECK: :[[@LINE-1]]:8: error: (S5.1)
+	}
+}
+
+int* change(int* ptr) {
+    int i = 0;
+    ptr = &i;
+//CHECK: :[[@LINE-1]]:9: error: (S5.1)
+
+    return ptr; //ok
+}
