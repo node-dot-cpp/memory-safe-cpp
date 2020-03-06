f19ffb8eb1062264b39eb29d459cfb5c8f24202e
Marcos Bracco <mb@o-log-n.com>
2020-03-06 19:51:10 -0300

    Rerun safe-library-db

--- checker/src/nodecpp-checker/nodecpp/RawPtrExprCheck.cpp
+++ checker/src/nodecpp-checker/nodecpp/RawPtrExprCheck.cpp
@@ -95,7 +95,7 @@ void RawPtrExprCheck::check(const MatchFinder::MatchResult &Result) {
     return;
   }
 
-  Ex->dumpColor();
+  // Ex->dumpColor();
   diag(Ex->getExprLoc(), "(S1) raw pointer expression not allowed");
 }
 
