9e3056db4a85bcb558c1a143feaf043203462793
Marcos Bracco <mb@o-log-n.com>
2020-05-08 17:08:20 -0300

    update iibmalloc, reverted fmt library

--- checker/tools/travis-windows.bat
+++ checker/tools/travis-windows.bat
@@ -15,7 +15,7 @@ cd build
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 @if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
-cmake -DCMAKE_CXX_COMPILER_LAUNCHER="sccache" -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_EXTERNAL_CHECKER_SOURCE_DIR=%cd%\..\checker -G Ninja ..\checker\3rdparty\llvm
+cmake -DCMAKE_CXX_COMPILER_LAUNCHER="C:\ProgramData\chocolatey\lib\sccache\tools\sccache.exe" -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_EXTERNAL_CHECKER_SOURCE_DIR=%cd%\..\checker -G Ninja ..\checker\3rdparty\llvm
 @if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
 ninja nodecpp-checker
