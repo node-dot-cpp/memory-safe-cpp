ecbc08609c26f15d5e10390a10e54436c4248dc9
Marcos Bracco <mb@o-log-n.com>
2020-07-22 15:17:40 -0300

    Travis debug

--- checker/tools/travis-windows-sccache-warmup.bat
+++ checker/tools/travis-windows-sccache-warmup.bat
@@ -30,7 +30,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliar
 
 echo %INCLUDE%
 
-cmake -DCMAKE_CXX_COMPILER_LAUNCHER="sccache.exe" -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_EXTERNAL_CHECKER_SOURCE_DIR=%cd%\..\checker -G Ninja ..\checker\3rdparty\llvm
+cmake -DCMAKE_CXX_COMPILER_LAUNCHER="sccache" -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_EXTERNAL_CHECKER_SOURCE_DIR=%cd%\..\checker -G Ninja ..\checker\3rdparty\llvm
 @if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
 ninja clangAST
--- checker/tools/travis-windows.bat
+++ checker/tools/travis-windows.bat
@@ -16,7 +16,7 @@ set PATH=C:\ProgramData\chocolatey\lib\sccache\tools\sccache-0.2.12-x86_64-pc-wi
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 @if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
-cmake -DCMAKE_CXX_COMPILER_LAUNCHER="sccache.exe" -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_EXTERNAL_CHECKER_SOURCE_DIR=%cd%\..\checker -G Ninja ..\checker\3rdparty\llvm
+cmake -DCMAKE_CXX_COMPILER_LAUNCHER="sccache" -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_EXTERNAL_CHECKER_SOURCE_DIR=%cd%\..\checker -G Ninja ..\checker\3rdparty\llvm
 @if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
 ninja nodecpp-checker
