24fe706e30ba965a5d93dd8ff2a99ce1ac5c03e8
Marcos Bracco <mb@o-log-n.com>
2021-05-31 13:20:16 -0300

    Fixes to EASTL from clang11

--- library/3rdparty/clone-EABase-EASTL.bat
+++ library/3rdparty/clone-EABase-EASTL.bat
@@ -5,6 +5,8 @@ rmdir /S /Q EASTL
 git clone --depth 1 -b 2.09.06 https://github.com/electronicarts/EABase.git
 @if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 cd EABase
+git apply ..\EABase-2.09.06.diff
+@if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 rmdir /S /Q .git
 cd ..
 
--- library/3rdparty/clone-EABase-EASTL.sh
+++ library/3rdparty/clone-EABase-EASTL.sh
@@ -6,6 +6,7 @@ rm -Rf EASTL
 
 git clone --depth 1 -b 2.09.06 https://github.com/electronicarts/EABase.git
 cd EABase
+git apply ../EABase-2.09.06.diff
 rm -Rf .git
 cd ..
 
