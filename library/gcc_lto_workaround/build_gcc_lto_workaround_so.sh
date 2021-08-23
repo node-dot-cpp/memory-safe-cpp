b82c64ecc6af6e11cc7a40afc676a7f640ea2cae
Marcos Bracco <mb@o-log-n.com>
2021-08-23 11:33:48 -0300

    testing -fno-lifetime-dse for gcc

--- library/gcc_lto_workaround/build_gcc_lto_workaround_so.sh
+++ library/gcc_lto_workaround/build_gcc_lto_workaround_so.sh
@@ -1,1 +1,1 @@
-g++ -I ../src/iibmalloc/src/foundation/include -I ../src/iibmalloc/src/foundation/3rdparty/fmt/include -shared -o libgcc_lto_workaround.so -fPIC gcc_lto_workaround.cpp
+g++-10 -I ../src/iibmalloc/src/foundation/include -I ../src/iibmalloc/src/foundation/3rdparty/fmt/include -shared -o libgcc_lto_workaround.so -fPIC gcc_lto_workaround.cpp
